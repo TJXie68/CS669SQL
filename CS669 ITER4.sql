329fb5d02431ad7dd93d4d6594013523439d075c
Tianjian Xie <111186868+TJXie68@users.noreply.github.com>
2023-11-16 18:15:56 -0500

    Add files via upload

--- CS669 ITER4.sql
+++ CS669 ITER4.sql
@@ -8,6 +8,7 @@ LastLogin TIMESTAMP NOT NULL, -- new
 MembershipStartDate DATE NOT NULL, -- new
 LanguagePerference VARCHAR(10) NOT NULL ); -- new
 
+
 CREATE TABLE Date(
 User_Date DECIMAL(32) PRIMARY KEY,
 Date DECIMAL(12) NOT NULL);
@@ -29,7 +30,6 @@ User_Date DECIMAL(32) NOT NULL,
 FOREIGN KEY(UserID) REFERENCES Account(UserID),
 FOREIGN KEY(User_Date) REFERENCES Date(User_Date));
 
-
 CREATE TABLE Cal(
 Calories DECIMAL(12),
 Score DECIMAL(12),
@@ -40,32 +40,36 @@ PRIMARY KEY (UserID, User_Date),
 FOREIGN KEY(UserID) REFERENCES Account(UserID),
 FOREIGN KEY(User_Date) REFERENCES Date(User_Date));
 
+
 CREATE TABLE V02MaxScore(
 UserID DECIMAL(12) NOT NULL,
 User_Date DECIMAL(32),
 AccountName VARCHAR(64) NOT NULL,
 VO2_Score DECIMAL(12),
-FOREIGN KEY(UserID, User_Date) REFERENCES Cal(UserID, User_Date));
+FOREIGN KEY(UserID, User_Date) REFERENCES Cal(UserID, User_Date),
+);
+
 
 CREATE TABLE BMIScore(
 UserID DECIMAL(12) NOT NULL,
 User_Date DECIMAL(32),
 BMI_Score DECIMAL(12),
-FOREIGN KEY(UserID, User_Date) REFERENCES Cal(UserID, User_Date));
+FOREIGN KEY(UserID, User_Date) REFERENCES Cal(UserID, User_Date)
+);
 
 CREATE TABLE PurchaseAccount(
 TrackingNumber VARCHAR(255) NOT NULL PRIMARY KEY, -- new
 UserID DECIMAL(12) NOT NULL,
+AccountName VARCHAR(64) NOT NULL,
 BMI DECIMAL(12) NOT NULL,
 Score DECIMAL(12) NOT NULL,
 Suggestion VARCHAR(255) NOT NULL,
 PurchaseDate DATE NOT NULL, -- new
-FOREIGN KEY(UserID) REFERENCES Account(UserID),
-FOREIGN KEY(BMI) REFERENCES BMI(BMI),
-FOREIGN KEY(Score, Suggestion) REFERENCES Cal(Score, Suggestion));
+FOREIGN KEY(UserID) REFERENCES Account(UserID)
+);
 
 CREATE TABLE FreeAccount(
 UserID DECIMAL(12) NOT NULL,
 AccountName VARCHAR(64) NOT NULL,
-FOREIGN KEY(UserID) REFERENCES Account(UserID),
-FOREIGN KEY(AccountName) REFERENCES Account(AccountName));
+FOREIGN KEY(UserID) REFERENCES Account(UserID)
+);
